ae4b007e2c4819787767eb99aa245f03b311585e
Daniel <toolsproed@gmail.com>
2024-09-01 18:53:39 +0300

    added alacritty config to script

--- setup.sh
+++ setup.sh
@@ -35,11 +35,32 @@ alias i=\"idiom\"
 alias cargo-update=\"cargo install-update -a\"
 alias rfmt=\"cargo fmt && cargo test\"
 
-# DAH SHELL END
-" >> $BASHRC
-    echo "DONE"
+# DAH SHELL END" >> $BASHRC
+    echo "Updated bashrc!"
     source $BASHRC
 else
-    echo "Could not find .bashrc in home dir!";
-    exit 1;
+    echo "Could not find $HOME/.bashrc in home dir!";
 fi
+
+if [[ -e $HOME/.config/ ]]; then
+    if [[ -e $HOME/.config/alacritty/alacritty.toml ]]; then
+        echo "skipping alacritty.toml it already exists!"
+    else
+        mkdir -p $HOME/.config/alacritty/
+        echo "
+[colors.primary]
+background = \"#000000\"
+
+[font]
+size = 14
+
+[window]
+dimensions = {columns = 160, lines = 40}
+decorations = \"None\"
+startup_mode = \"Maximized\"" > $HOME/.config/alacritty/alacritty.toml
+        echo "Create alacritty.toml with presets!"
+    fi
+else
+    echo "Could not find $HOME/.config/ directory!"
+fi
+echo "FINISHED!"
